c62c38b7cd638c4131a78cf87971f4bb247f937a
Wayne Burris <waterseeker@Hotmail.com>
2021-08-02 19:42:33 -0500

    changed display text for main repo link on RepoData

--- display-github-repos/src/RepoData.tsx
+++ display-github-repos/src/RepoData.tsx
@@ -1,12 +1,14 @@
 import { Issue } from './Issue'
 
-const RepoData = ({ html_url, title, user, repository_url, id}:Issue) => {       
+
+const RepoData = ({ html_url, title, user, repository_url, id}:Issue) => {  
+    let mainRepoName = html_url.split('/')[4]     
     return (
         <div key={id}>
             <p>Title: <a href={html_url}>{title}</a></p>
             <p>Author: <a href={user.html_url}>{user.login}</a></p>
             <p>Avatar: <a href={user.html_url}>{user.avatar_url}</a></p>
-            <p>Parent Repo: <a href={html_url.split('/issues')[0]}>{repository_url}</a></p>
+            <p>Parent Repo: <a href={html_url.split('/issues')[0]}>{mainRepoName}</a></p>
         </div>
     )
 }
